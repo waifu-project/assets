d020a2f2ea097fb2f3468b447ca4b14c42fdb590
龙馨竹 <chenhonzhou@gmail.com>
2022-06-03 13:16:58 +0800

    feat: 添加背景图片

--- yy/src/App.vue
+++ yy/src/App.vue
@@ -3,9 +3,26 @@
 </script>
 
 <template>
+  <div id="bg" >
+    <img src="@/assets/waifu.png" />
+  </div>
   <router-view></router-view>
 </template>
 
-<style>
-
+<style scoped>
+#bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  opacity: .6;
+}
+#bg img {
+  width: 100%;
+  height: 100%;
+}
 </style>
