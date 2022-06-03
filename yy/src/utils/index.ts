bbc0803cd9e08f6df17f9f0a268143a344964fcb
龙馨竹 <chenhonzhou@gmail.com>
2022-06-03 23:29:00 +0800

    feat: 生成镜像文件

--- yy/src/utils/index.ts
+++ yy/src/utils/index.ts
@@ -1,3 +1,9 @@
+import FileSaver from './filesave.js'
 export default function isObject(val: any) {
   return val != null && typeof val === 'object' && Array.isArray(val) === false;
 };
+
+export function exportFile(fileName: string, content: string){
+  var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+  (FileSaver as any).saveAs(blob, fileName);
+}
